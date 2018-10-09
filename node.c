8a792ca629e10d213cae15267a8d32925a5924cf
canozo <canwoahzo@gmail.com>
2018-10-09 12:56:35 -0600

    Nuevos tipos de nodos

--- node.c
+++ node.c
@@ -8,6 +8,7 @@ node* new_node_binary_op(node* left, token* token, node* right) {
     this->left = left;
     this->token = token;
     this->right = right;
+    this->child_count = 0;
 
     return this;
 }
@@ -19,6 +20,7 @@ node* new_node_unary_op(token* token, node* right) {
     this->left = NULL;
     this->token = token;
     this->right = right;
+    this->child_count = 0;
 
     return this;
 }
@@ -31,10 +33,61 @@ node* new_node_number(token* token) {
     this->left = NULL;
     this->token = token;
     this->right = NULL;
+    this->child_count = 0;
 
     return this;
 }
 
+node* new_node_assign(node* left, token* token, node* right) {
+    node* this = malloc(sizeof(node));
+
+    this->type = N_ASSIGN;
+    this->left = left;
+    this->token = token;
+    this->right = right;
+    this->child_count = 0;
+
+    return this;
+}
+
+node* new_node_variable(token* token) {
+    node* this = malloc(sizeof(node));
+
+    this->type = N_VARIABLE;
+    this->left = NULL;
+    this->token = token;
+    this->right = NULL;
+    this->child_count = 0;
+
+    return this;
+}
+
+node* new_node_compound() {
+    node* this = malloc(sizeof(node));
+
+    this->type = N_COMPOUND;
+    this->left = NULL;
+    this->token = NULL;
+    this->right = NULL;
+    this->child_count = 0;
+
+    return this;
+}
+
+node* new_node_empty() {
+    node* this = malloc(sizeof(node));
+
+    this->type = N_EMPTY;
+    this->left = NULL;
+    this->token = NULL;
+    this->right = NULL;
+    this->child_count = 0;
+
+    return this;
+}
+
+
 void delete_node(node* this) {
+    // TODO free children
     free(this);
 }
--- node.h
+++ node.h
@@ -4,6 +4,10 @@
 #define N_NUMBER 0
 #define N_BINARY_OP 1
 #define N_UNARY_OP 2
+#define N_ASSIGN 3
+#define N_VARIABLE 4
+#define N_COMPOUND 5
+#define N_EMPTY 5
 
 #include "token.h"
 
@@ -12,11 +16,17 @@ typedef struct node {
     struct node* left;
     token* token;
     struct node* right;
+    struct node* children[128];
+    int child_count;
 } node;
 
 node* new_node_binary_op(node*, token*, node*);
 node* new_node_unary_op(token*, node*);
 node* new_node_number(token*);
+node* new_node_assign(node*, token*, node*);
+node* new_node_variable(token*);
+node* new_node_compound();
+node* new_node_empty();
 void delete_node(node*);
 
 #endif
