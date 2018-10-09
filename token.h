754a7b868b141ec01c1faef77e1cc00d97ccbe43
canozo <canwoahzo@gmail.com>
2018-10-09 12:55:00 -0600

    Nuevos tokens

--- token.h
+++ token.h
@@ -10,6 +10,12 @@
 #define T_PARENTHESES_OPEN 6
 #define T_PARENTHESES_CLOSE 7
 #define T_UNKNOWN 8
+#define T_ASSIGN 9
+#define T_KEYWORD 10
+#define T_VARIABLE 11
+#define T_SEMICOLON 12
+#define T_BEGIN 13
+#define T_END 14
 
 typedef struct token {
     int type;
